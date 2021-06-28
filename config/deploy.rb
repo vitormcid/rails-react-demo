07c0338b72a186622762ffbe147c4a14b549cf02
Vitor Cid <cid.vitor@hotmail.com>
2021-06-27 21:24:50 -0300

    minor fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_to, "/home/deploy/www/rails-react-demo"
 
 # Default value for :linked_files is []
 append :linked_files, "config/database.yml",  "config/master.key"
-append :linked_files, 'config/database.yml', 'config/secrets.yml', '.env'
+append :linked_files, 'config/database.yml',  '.env'
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
 
